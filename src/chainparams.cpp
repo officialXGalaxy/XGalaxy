43634ee4657b53c54dac1f55e7ffeaa6b02b08c9
tri <tri282@gmail.com>
2018-08-19 09:24:07 -0700

    change mining require peers to true

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -177,7 +177,7 @@ public:
         // XGalaxy BIP44 coin type is '5'
         base58Prefixes[EXT_COIN_TYPE]  = boost::assign::list_of(0x80)(0x00)(0x00)(0x05).convert_to_container<std::vector<unsigned char> >();
 
-        fMiningRequiresPeers = false;
+        fMiningRequiresPeers = true;
         fDefaultConsistencyChecks = false;
         fRequireStandard = true;
         fMineBlocksOnDemand = false;
